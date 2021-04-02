26cd5537c31c38058bd6a5a5d244ee53b98d9c02
I.N <48551497+leztien@users.noreply.github.com>
2021-04-02 20:22:56 +0200

    Update make_data_for_classification.py

--- make_data_for_classification.py
+++ make_data_for_classification.py
@@ -7,6 +7,65 @@ make n-dimensional dataset with k lineraly seperable classes (n-dimensional "hyp
 import numpy as np
 
 
+def make_blobs(n_blobs=3, n_points=100): 
+    def make_blob(m=100, center=(0,0), span=10):
+        while(True):
+            with np.warnings.catch_warnings():
+                np.warnings.filterwarnings("error")  # treat warning as errors
+                try: 
+                    Σ = np.random.randint(low=-span, high=span, size=(2,2))
+                    Σ[1,0] = Σ[0,1]
+                    mx = np.random.multivariate_normal(mean=center, cov=Σ, size=m)
+                    b = np.unique(Σ).size <= 2
+                    if b: continue
+                    else: break
+                except RuntimeWarning: continue
+        return(mx)
+
+
+    def get_center_and_radius_of_blob(mx):
+        center = (mx.max(axis=0) + mx.min(axis=0))/2
+        ix = ((mx - center)**2).sum(axis=1).argmax()
+        max_point = mx[ix]
+        radius = np.sqrt(((max_point - center)**2).sum())
+        return(center,radius)
+        
+    
+    def shift_blob(main_blob, other_blob):
+        from math import pi as π, cos, sin
+        from random import uniform
+        
+        center, radius = main_blob[:2] if isinstance(main_blob, (tuple,list)) else get_center_and_radius_of_blob(main_blob)
+        center2, radius2 = get_center_and_radius_of_blob(other_blob)
+        
+        θ = uniform(0, 2*π)
+        
+        adjacent = (radius+radius2) * cos(θ)
+        opposite = (radius+radius2) * sin(θ)
+        
+        x,y = np.add(center, [adjacent, opposite]) - center2
+    
+        other_blob += (x,y)
+        return(other_blob)
+    
+    X = []
+    y = sum(([label,]*n_points for label in range(n_blobs)),[])
+      
+        
+    mx = make_blob(n_points)
+    X.append(mx)
+    center, radius = get_center_and_radius_of_blob(mx)
+    
+    for i in range(1, n_blobs):
+        mx_new = shift_blob([center, radius], make_blob(n_points))
+        X.append(mx_new)
+    
+    X = np.concatenate(X, axis=0)
+    X += np.abs(X.min(axis=0))
+    return(X, np.array(y, dtype='uint8'))
+
+
+
 def make_blob(m, n, random_state=None, return_radius=False):
     rs = random_state or globals().get("rs", np.random.randint(1,999))
     rs = rs if isinstance(rs, np.random.mtrand.RandomState) else np.random.RandomState(int(rs))
@@ -62,8 +121,12 @@ def make_data_for_classification(m:'total number of data-points',
     The structure of the data and how it fills the space can be visualized in 3d (if n==3)
     (the data is unnormalized/unstandardized and is located in the positive hyper-quadrant)"""
 
-    if n < 3: raise ValueError("n must be 3 or higher")
-
+    if n < 2: raise ValueError("n must be 2 or higher")
+    
+    # If 2D
+    if n == 2:
+        return make_blobs(k, m)
+    
     #calculate numbers of data-points in each blob/class
     from numbers import Integral, Real
     from collections.abc import Sequence
