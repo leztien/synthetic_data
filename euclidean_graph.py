39d2df2c23a17bf4e365afdeea236c51e596801b
Ilja Neitzel <48551497+leztien@users.noreply.github.com>
2024-06-23 16:32:04 +0200

    Update euclidean_graph.py

--- euclidean_graph.py
+++ euclidean_graph.py
@@ -10,7 +10,8 @@ and draw that graph
 """
 
 
-from math import atan, degrees
+from math import atan, degrees, sqrt, ceil
+from itertools import product
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,6 +24,24 @@ def generate_graph(n=10, random_state=None):
     Returns a dictionary in this form:
     graph = {'vertices': {"A": (x, y), "B": (x, y)}, 'edges': {("A","B"): distance}}
     """
+    
+    def generate_grid_points(n, height=10, width=15, random_state=None):
+        """make a coordinates matrix of random points based on a grid"""
+        rs = np.random.RandomState(random_state) if type(random_state) in (int, type(None)) else random_state
+        side_factor = width / height
+        side = sqrt(n / side_factor)
+        h, w = ceil(side), ceil(side*side_factor)
+        
+        xx = [width / w * i for i in range(w+1)]
+        yy = [height / h * i for i in range(h+1)]
+        
+        coordinates_matrix = np.array(list(product(xx, yy))[:n])
+        coordinates_matrix += rs.normal(0, scale=width/20, size=(n,2))
+        coordinates_matrix -= coordinates_matrix.min(axis=0) - 1
+        coordinates_matrix = coordinates_matrix // 0.5 * 0.5
+        assert len(coordinates_matrix) == n
+        return coordinates_matrix
+    
     # random state
     random_state = int(random_state or np.random.randint(1, 9999))
     print("random state:", random_state)
@@ -38,7 +57,11 @@ def generate_graph(n=10, random_state=None):
     graph = {'vertices': dict(), 'edges': dict()}
     
     # generate coordinates of the n points
-    coordinates_matrix = (rs.rand(n, 2) * span) // 0.5 * 0.5
+    if rs.rand() < 0.5:
+        coordinates_matrix = (rs.rand(n, 2) * span) // 0.5 * 0.5
+    else:
+        coordinates_matrix = generate_grid_points(n, random_state=rs)
+    
     graph['vertices'] = {chr(65 + i): (x,y) for i, (x,y) in enumerate(coordinates_matrix)}
     
     # generate edges
@@ -56,11 +79,9 @@ def generate_graph(n=10, random_state=None):
         for j in np.nonzero(row)[0]:
             distance = ((coordinates_matrix[i] - coordinates_matrix[j]) ** 2).sum() ** 0.5
             graph['edges'][(chr(65 + i), chr(65 + j))] = round(distance)
-    
     return graph
 
     
-
 def draw_graph(graph, path=None):
     """Draws a geometric graph from the given graph dictionary"""
     
@@ -112,9 +133,6 @@ def draw_graph(graph, path=None):
     if path:
         points = [graph['vertices'][k] for k in path]
         for i in range(len(points)-1):
-            print(points[i], points[i+1])
-            if (path[i], path[i+1]) not in graph['edges']:
-                raise ValueError(f"The edge {(path[i], path[i+1])} is not in the graph.")
             plot_line(points[i], points[i+1], color='red')
             annotate_line(points[i], points[i+1], text=f"{i+1}", weight='bold')
     
@@ -131,10 +149,5 @@ if __name__ == '__main__':
     draw_graph(graph)
     
     
-    # demo 2
-    plt.figure()
-    graph = generate_graph(7, random_state=7471)
-    
-    solution_path = ("A", "D", "G")
-    ax = draw_graph(graph, path=solution_path)
+
 
